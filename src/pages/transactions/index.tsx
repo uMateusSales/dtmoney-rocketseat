ea12d88eb82210d30de1b1451b42af5cb4551b0a
Mateus Sales <uMateusSales@github.com>
2023-10-17 00:36:30 -0300

    feat: table de transações

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import Summary from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsTable,
+  TrasactionContainer,
+} from "./styles";
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+      <TrasactionContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <PriceHighlight variant="entrada">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="saida">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TrasactionContainer>
     </div>
   );
 };
